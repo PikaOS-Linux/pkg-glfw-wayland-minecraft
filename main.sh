b1a5e5541b3b044a87853b4772ba7dafcc6c89b2
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-01-06 19:53:49 +0300

    update to git

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ DEBIAN_FRONTEND=noninteractive
 git clone https://github.com/glfw/glfw ./glfw-wayland-minecraft
 cp -rvf ./debian ./glfw-wayland-minecraft
 cd ./glfw-wayland-minecraft
-git reset --hard 87d5646f5d2bad0562744501633bf8105f59c193
+#git reset --hard 87d5646f5d2bad0562744501633bf8105f59c193
 for i in $(cat ../patches/series) ; do echo "Applying Patch: $i" && patch -Np1 -i ../patches/$i || bash -c "echo "Applying Patch $i Failed!" && exit 2"; done
 
 # Get build deps
